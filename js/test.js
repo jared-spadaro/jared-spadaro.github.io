6084d5639c87050c7b1b1f7e3d060929822ed75d
Jared Spadaro <jarspada@cisco.com>
2020-11-14 08:45:03 -0800

    adding react support

---
+++ js/test.js
@@ -0,0 +1,7 @@
+// Display a "Like" <button>
+function LikeButton() {
+  return <button onClick={() => this.setState({ liked: true })}>Like</button>;
+}
+
+const domContainer = document.querySelector("#react-container");
+ReactDOM.render(LikeButton, domContainer);
