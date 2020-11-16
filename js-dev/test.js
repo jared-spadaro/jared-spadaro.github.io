78bab963bb14ea18a1101faa1d86979a1c080c6a
Jared Spadaro <jarspada@cisco.com>
2020-11-16 12:03:44 -0800

    react

--- js-dev/test.js
+++ js-dev/test.js
@@ -1,11 +1,25 @@
-// Display a "Like" <button>
-function LikeButton() {
-  return (
-    <button 
+"use strict";
+
+import ReactDOM from "react-dom";
+import react from "react";
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return "You liked this.";
+    }
+    return (
+      <button 
         onClick={() => this.setState({ liked: true })}>
         Like
-    </button>
-  );
+      </button>
+    );
+  }
 }
 
 const domContainer = document.querySelector("#react-container");
