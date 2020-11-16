4eeb839c0c365261ed68aa8149b556c8bc3a622f
Jared Spadaro <jarspada@cisco.com>
2020-11-16 13:24:44 -0800

    paths

--- js-prod/test.js
+++ js-prod/test.js
@@ -8,8 +8,8 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-import ReactDOM from "react-dom";
-import react from "react";
+import ReactDOM from "/react-dom";
+import react from "/react";
 
 var LikeButton = function (_React$Component) {
   _inherits(LikeButton, _React$Component);
