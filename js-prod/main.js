6e65c5a6b0ff5c1ed74c149320289ae7fe659a8f
Jared Spadaro <jarspada@brussels.home>
2020-12-23 11:57:41 -0800

    test

--- js-prod/main.js
+++ js-prod/main.js
@@ -8,7 +8,7 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-import { Document, Page } from 'react-pdf';
+import { Document, Page } from 'node_modules/react-pdf';
 
 var NUM_SQUARES = 400;
 var RIGHT = 1;
